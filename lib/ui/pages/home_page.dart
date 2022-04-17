58675fb19e5722cd143f96a8108fcfef2d2bfe67
ShingoKobayashi <>
2022-04-18 00:46:50 +0900

    Add animal page tab

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import '../../entity/data.dart';
 import '../../main.dart';
+import '../widgets/Page1.dart';
+import '../widgets/Page2.dart';
+import '../widgets/Page3.dart';
+import '../widgets/Page4.dart';
+import '../widgets/animal_kinds_tab_bar.dart';
 import '../widgets/custom_sliver_app_bar.dart';
 import '../widgets/video_card.dart';
 
@@ -12,6 +17,9 @@ class HomePage extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           CustomSliverAppBar(),
+          new SliverFillRemaining(
+              child: AnimalKindsTabBar("ねこ", Page1())
+          ),
           SliverPadding(
             padding: const EdgeInsets.only(bottom: 60.0),
             sliver: SliverList(
